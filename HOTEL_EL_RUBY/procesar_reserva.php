0d8b67bb0620a67b44001deaeb964c2e7c10b8d8
jhomarioxd <jhoonmarioxd2104mail.com>
2023-09-19 14:22:54 -0500

    actulizacion de diseño logos y restablecer conexion de base de datos con php

--- HOTEL_EL_RUBY/procesar_reserva.php
+++ HOTEL_EL_RUBY/procesar_reserva.php
@@ -1,9 +1,9 @@
 <?php
-// Establecer la conexión con la base de datos MySQL
+// Establecer la conexión a la base de datos
 $servername = "127.0.0.1";
 $username = "root";
 $password = "";
-$dbname = "hotelruby";
+$dbname = "hotelelruby";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -21,8 +21,14 @@ $fecha_salida = $_POST["fecha_salida"];
 $habitacion = $_POST["habitacion"];
 $personas = $_POST["personas"];
 
-// Crear la tabla si no existe
-$sql_create_table = "CREATE TABLE IF NOT EXISTS reservas (
+// Verificar que los campos no estén vacíos (puedes agregar más validaciones según tus necesidades)
+if (empty($nombre) || empty($email) || empty($telefono) || empty($fecha_llegada) || empty($fecha_salida) || empty($habitacion) || empty($personas)) {
+    echo "Todos los campos son obligatorios. Por favor, completa el formulario.";
+    exit;
+}
+
+// Crear la tabla si no existe (si ya existe, esta línea no tiene efecto)
+$sql_create_table = "CREATE TABLE IF NOT EXISTS reservar (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
@@ -35,18 +41,22 @@ $sql_create_table = "CREATE TABLE IF NOT EXISTS reservas (
 
 if ($conn->query($sql_create_table) === FALSE) {
     echo "Error al crear la tabla: " . $conn->error;
+    exit;
 }
+
 
 // Insertar los datos en la tabla
 $sql_insert_data = "INSERT INTO reservas (nombre, email, telefono, fecha_llegada, fecha_salida, habitacion, personas)
-    VALUES ('$nombre', '$email', '$telefono', '$fecha_llegada', '$fecha_salida', $habitacion, $personas)";
+    VALUES ('$nombre', '$email', '$telefono', '$fecha_llegada', '$fecha_salida', '$habitacion', '$personas')";
 
 if ($conn->query($sql_insert_data) === TRUE) {
     echo "Reserva realizada con éxito";
 } else {
     echo "Error al realizar la reserva: " . $conn->error;
 }
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+phpinfo();
 // Cerrar la conexión
 $conn->close();
 ?>
